728e9656c66689849793d6f05e7871459c3d960d
koluyantrez <210447@umons.ac.be>
2025-06-16 14:40:47 +0200

    base table

---
+++ src/main/java/be/ac/umons/razanajao/sddproject/other/Table.java
@@ -0,0 +1,52 @@
+package be.ac.umons.razanajao.sddproject.other;
+
+public class Table {
+
+    int x,y;
+    String[] header;
+    String[][] data;
+
+    public Table(int x, int y){
+        this.x=x;
+        this.y=y;
+        this.header = new String[y];
+        this.data = new String[x][y];
+    }
+
+
+
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public String[] getHeader(){
+        return this.header;
+    }
+
+    public String[][] getData(){
+        return this.data;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setHeader(String[] header) {
+        this.header = header;
+    }
+
+    public void setData(String[][] data) {
+        this.data = data;
+    }
+
+
+}
