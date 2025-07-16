ba66fbd994529ec53d7dd22ab6b91c1802634067
koluyantrez <210447@umons.ac.be>
2025-07-16 15:04:32 +0200

    javadoc

--- src/main/java/be/ac/umons/razanajao/sddproject/frontend/Hermes.java
+++ src/main/java/be/ac/umons/razanajao/sddproject/frontend/Hermes.java
@@ -2,13 +2,27 @@ package be.ac.umons.razanajao.sddproject.frontend;
 
 import be.ac.umons.razanajao.sddproject.TestOrthogonalRangeSearching;
 
-
+/**
+ * It just calls two fonctions from the main class for the frontend. It may be compared as an API because all file
+ * in the folder "frontend" do a link between backend and the main class. This is more coherent.
+ *
+ */
 public class Hermes {
 
+    /**
+     * It calls the function "greenCode" from the main class.
+     *
+     * @param msg   The message to display.
+     */
     public static void green(String msg){
         TestOrthogonalRangeSearching.greenCode(msg);
     }
 
+    /**
+     * It calls the function "redCode" from the main class.
+     *
+     * @param msg   The message to display.
+     */
     public static void red(String msg){
         TestOrthogonalRangeSearching.redCode(msg);
     }
