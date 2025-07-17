c5ae400a2d261bb4f5b34cf86ccbb38d72082939
koluyantrez <210447@umons.ac.be>
2025-07-17 14:17:41 +0200

    ABR

---
+++ src/main/java/be/ac/umons/razanajao/sddproject/structure/BSTree.java
@@ -0,0 +1,28 @@
+package be.ac.umons.razanajao.sddproject.structure;
+
+/**
+ * Describes a binary search tree. It extends Tree class. Taken from moodle and adapted for KdTree.
+ *
+ * @param <D> the type of the data stored in the tree that extends Comparable.
+ */
+public class BSTree<D> extends Tree<D>{
+
+    // Constructors
+    public BSTree() {
+        super();
+    }
+
+    public BSTree(D d, BSTree<D> l, BSTree<D> r) {
+        super(d, l, r);
+    }
+
+    // Redefine to avoid constant casting
+    public BSTree<D> getLeft() {
+        return (BSTree<D>) super.getLeft();
+    }
+
+    public BSTree<D> getRight() {
+        return (BSTree<D>) super.getRight();
+    }
+
+}
